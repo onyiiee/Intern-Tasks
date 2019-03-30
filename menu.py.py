7932be361cdcf2d589bc7497901c1ceb38c4b2ed
onyiiee <45360319+onyiiee@users.noreply.github.com>
2019-03-30 01:10:29 +0100

    Update menu.py.py

--- menu.py.py
+++ menu.py.py
@@ -2,9 +2,10 @@ menu ={'sharwarma':1200,'semovita and egusi':1600,'afang soup':1400,'coke':300,}
 order_made =[]
 cost_of_each =[]
 delivery_fee =900
-name =str(input("\nwhat is your name"))
-location =input("\nwhat is your location")
-phone_number =int(input("\nwhat is your phone number"))
+name =str(input("\nwhat is your name "))
+address =input("\nwhat is your address "))
+email =str(input("\nwhat is your email address "))
+phone_number =int(input("\nwhat is your phone number "))
 numbering = 0
 print('\n\nHello ' + name + 'Welcome to Fruittie Kitchen' + '\nhere is our menu')
 for food, price in menu.items():
